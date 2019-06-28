15748b0faef908a145fa0969f4ded488d391ee7c
shruti <shrutidpatil12@gmail.com>
2019-06-28 11:34:05 +0530

    Prototype

---
+++ DesignPattern/Prototype/Employees.java
@@ -0,0 +1,39 @@
+package Prototype;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Employees implements Cloneable
+{
+private List<String> empList;
+	
+	public Employees(){
+		empList = new ArrayList<String>();
+	}
+	
+	public Employees(List<String> list){
+		this.empList=list;
+	}
+	public void loadData(){
+	
+		empList.add("supriya");
+		empList.add("samiksha");
+		empList.add("prince");
+		empList.add("capgemini");
+	}
+	
+	public List<String> getEmpList() {
+		return empList;
+	}
+
+	@Override
+	public Object clone() throws CloneNotSupportedException{
+			List<String> temp = new ArrayList<String>();
+			for(String s : this.getEmpList()){
+				temp.add(s);
+			}
+			return new Employees(temp);
+	}
+	
+}
