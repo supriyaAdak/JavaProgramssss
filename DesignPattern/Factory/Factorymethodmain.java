a422ec5da75c4e2e278d5396a516b812803d69b7
shruti <shrutidpatil12@gmail.com>
2019-06-28 11:32:01 +0530

    Factory

---
+++ DesignPattern/Factory/Factorymethodmain.java
@@ -0,0 +1,17 @@
+package Factory;
+
+public class Factorymethodmain 
+{
+public static void main(String[] args) {
+	Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+	
+	Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+	Computer laptop = ComputerFactory.getComputer("laptop", "3 GB", "160GB", "2.4 GHz");
+	System.out.println("Factory PC Config::"+pc);
+	System.out.println();
+	System.out.println("Factory Server Config::"+server);
+	System.out.println();
+	System.out.println("Factory LAPTOP Config::"+laptop);
+	System.out.println();
+}
+}
---
+++ DesignPattern/Factory/Laptop.java
@@ -0,0 +1,29 @@
+package Factory;
+
+public class Laptop extends Computer{
+	private String ram;
+	private String hdd;
+	private String cpu;
+	public Laptop(String ram, String hdd, String cpu){
+		this.ram=ram;
+		this.hdd=hdd;
+		this.cpu=cpu;
+		}
+	@Override
+	public String getRAM() {
+		// TODO Auto-generated method stub
+		return this.ram;
+	}
+
+	@Override
+	public String getHDD() {
+		// TODO Auto-generated method stub
+		return this.hdd;
+	}
+
+	@Override
+	public String getCPU() {
+		// TODO Auto-generated method stub
+		return this.cpu;
+	}
+}
